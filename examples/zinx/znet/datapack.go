c214bc65e71df1064374471089190e8ed29781f1
kaidev1024 <58545754+kaidev1024@users.noreply.github.com>
2020-10-06 22:20:16 -0700

    add datapack

--- examples/zinx/znet/datapack.go
+++ examples/zinx/znet/datapack.go
@@ -1,1 +1,62 @@
 package znet
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+
+	"github.com/kaidev1024/gokai/zinx/utils"
+	"github.com/kaidev1024/gokai/zinx/ziface"
+)
+
+type DataPack struct{}
+
+func NewDataPack() *DataPack {
+	return &DataPack{}
+}
+
+func (dp *DataPack) GetHeadLen() uint32 {
+	return 8
+}
+
+func (dp *DataPack) Pack(msg ziface.IMessage) ([]byte, error) {
+	dataBuff := bytes.NewBuffer([]byte{})
+
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetMsgLen()); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetMsgId()); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetData()); err != nil {
+		return nil, err
+	}
+
+	return dataBuff.Bytes(), nil
+}
+
+func (dp *DataPack) Unpack(binaryData []byte) (ziface.IMessage, error) {
+	dataBuff := bytes.NewReader(binaryData)
+
+	msg := &Message{}
+
+	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.DataLen); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.Id); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.DataLen); err != nil {
+		return nil, err
+	}
+
+	if utils.GlobalObject.MaxPackageSize > 0 && msg.DataLen > utils.GlobalObject.MaxPackageSize {
+		return nil, errors.New("too large msg data recv!")
+	}
+
+	return msg
+}
